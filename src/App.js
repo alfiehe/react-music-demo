ba31e5d045a18a57a8456c464f2dd52c2dc93341
alfiehe <alfiehe@tencent.com>
2020-12-12 22:19:19 +0800

    Redux Ready

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
 import { HashRouter } from 'react-router-dom';
 import { IconStyle } from './assets/iconfont/iconfont';
 import { GlobalStyle } from './style';
 import routes from './routes';
+import store from './store';
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle></GlobalStyle>
-      <IconStyle></IconStyle>
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
