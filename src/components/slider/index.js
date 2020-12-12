5b0fd23a4c369bbfb020fc7151cbe7d8df92095e
alfiehe <alfiehe@tencent.com>
2020-12-12 23:14:23 +0800

    Swiper slider

---
+++ src/components/slider/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import SwiperCore, { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css'
+import { SliderContainer } from './style';
+
+// install Swiper components
+SwiperCore.use([Pagination]);
+
+function Slider({ bannerList }) {
+  return (
+    <SliderContainer>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        {
+          bannerList.map((slider, index) => {
+            return (
+              <SwiperSlide className="swiper-slide" key={slider.imageUrl + index}>
+                <img src={slider.imageUrl} width="100%" height="100%" alt="推荐" />
+              </SwiperSlide>
+            );
+          })
+        }
+      </Swiper>
+    </SliderContainer>
+  );
+}
+
+export default React.memo(Slider);
---
+++ src/components/slider/style.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+export const SliderContainer = styled.div`
+  position: relative;
+`
